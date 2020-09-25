6270991ff268171ff1fe85d3efb7ca597029adcc
刘建佐 <liujianzuo@gmail.com>
2020-09-25 11:15:38 +0800

    删除

--- mov/fanren_start/command.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -i "/Users/liujianzuo/Downloads/视频/ttt/piantou.mp4" -vcodec copy -acodec aac -bsf:v h264_mp4toannexb -map 0:v:0 -map 0:a? -f segment -segment_list out.m3u8  -segment_time 10 out%05d.ts
